5366b67a7d0b7c33067b8bb43042db5846524ecc
Mouse0226 <mouse1987.ml@gmail.com>
2022-01-21 15:07:14 -0600

    save icons

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,29 @@
 
+var setCurrentDay = function() {
+    var dateText = moment().format('dddd, MMMM D, YYYY');    
+    var dateEl = $('<p>').addClass('lead').text(dateText);
+
+    $('#currentDay').append(dateEl);
+}
+
 var auditTime = function(textEl) {
 
     var time = $(textEl).find('.hour').text();
     var rowTime = moment(time, 'hA');
+    var textBox = $(textEl).find('.description')
 
     $(textEl).removeClass('past present future');
 
-    console.log(rowTime);
-    console.log(moment().format('hA'));
-
     if (moment().isAfter(rowTime)) {
-        $(textEl).addClass("past");
+        $(textBox).addClass("past");
     } else if (moment().isSame(rowTime)) {
-        $(textEl).addClass("present");
+        $(textBox).addClass("present");
     } else {
-        $(textEl).addClass("future");
+        $(textBox).addClass("future");
     }
 };
 
-
-
-
+setCurrentDay();
 
 setInterval(function() {
     $(".row").each(function(index, el) {
